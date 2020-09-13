93f8ca388f85874bd986fdc48bf810e04083cc73
Guillaume Sautière <guillaume.sautiere@gmail.com>
2020-09-13 13:26:16 +0200

    use log scale, plot values

--- notebooks/clean.py
+++ notebooks/clean.py
@@ -220,13 +220,35 @@ data_prize_cars
 
 palette = sns.color_palette("husl", 8)
 
+
+# +
+# taken from https://stackoverflow.com/a/51535326/5317241
+
+def show_values_on_bars(axs):
+    def _show_on_single_plot(ax):
+        num_patches = len(ax.patches)
+        for p in ax.patches:
+            _x = p.get_x() + 4 * p.get_width() / 5
+            _y = p.get_y() + p.get_height() / 2 + 0.03
+            value = '{:.0f}'.format(p.get_width())
+            if value == '1':
+                _x += .055
+            ax.text(_x, _y, value, ha="center", fontsize=14, color='white', weight='bold', fontname='Roboto') 
+    if isinstance(axs, np.ndarray):
+        for idx, ax in np.ndenumerate(axs):
+            _show_on_single_plot(ax)
+    else:
+        _show_on_single_plot(axs)
+
+
 # +
 fig, ax = plt.subplots(figsize=(8, 8))
 sns.set_theme(style="whitegrid", font='Roboto')
 
 # bar plot
-sns.barplot(y='Manufacturing Country', x='# cars featured', data=data_cars, color=palette[0], label='All')
-sns.barplot(y='Manufacturing Country', x='# cars featured', data=data_prize_cars, color=palette[1], label='Prize')
+sns.barplot(y='Manufacturing Country', x='# cars featured', data=data_cars, color=palette[0], label='All', log=True)
+sns.barplot(y='Manufacturing Country', x='# cars featured', data=data_prize_cars, color=palette[1], label='Prize', log=True)
+show_values_on_bars(ax)
 
 # title
 ax.text(x=0.0, y=1.05, s='Distribution of cars per country of origin in GT2', fontsize=18, weight='bold', ha='left', va='bottom', transform=ax.transAxes)
@@ -241,6 +263,3 @@ plt.yticks(fontsize=14)
 plt.xticks(fontsize=14)
 plt.legend(fontsize=16)
 plt.show()
-# -
-
-
